a21903646ab0ad2e732566c69d77cde7cbd1bc6c
Ying Xiong <yxiong@dropbox.com>
2015-12-25 08:28:36 -0800

    Cleanup MapViewController and add outlet

--- Virtual Tourist/MapViewController.swift
+++ Virtual Tourist/MapViewController.swift
@@ -6,20 +6,15 @@
 //  Copyright © 2015 Ying Xiong. All rights reserved.
 //
 
+import MapKit
 import UIKit
 
 class MapViewController: UIViewController {
 
+    @IBOutlet weak var mapView: MKMapView!
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
-
 }
 
