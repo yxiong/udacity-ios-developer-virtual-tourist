9040f7f9848f72b48c0e85a66f8602bbfa10b39d
Ying Xiong <yxiong@dropbox.com>
2015-12-26 15:55:02 -0800

    Move getImageURLs to FlickrPhotoDownloader

--- Virtual Tourist/FlickrPhotoDownloader.swift
+++ Virtual Tourist/FlickrPhotoDownloader.swift
@@ -23,6 +23,85 @@ class FlickrPhotoDownloader {
     let LON_MIN = -180.0
     let LON_MAX = 180.0
 
+    func getImageURLsFromFlickrWithPage(methodArguments: [String : AnyObject], pageNumber: Int, completion:  (([NSURL]) -> Void)?) {
+        /* Add the page to the method's arguments */
+        var withPageDictionary = methodArguments
+        withPageDictionary["page"] = pageNumber
+
+        let session = NSURLSession.sharedSession()
+        let urlString = BASE_URL + escapedParameters(withPageDictionary)
+        let url = NSURL(string: urlString)!
+        let request = NSURLRequest(URL: url)
+
+        let task = session.dataTaskWithRequest(request) { (data, response, error) in
+
+            /* GUARD: Was there an error? */
+            guard (error == nil) else {
+                print("There was an error with your request: \(error)")
+                return
+            }
+
+            /* GUARD: Did we get a successful 2XX response? */
+            guard let statusCode = (response as? NSHTTPURLResponse)?.statusCode where statusCode >= 200 && statusCode <= 299 else {
+                if let response = response as? NSHTTPURLResponse {
+                    print("Your request returned an invalid response! Status code: \(response.statusCode)!")
+                } else if let response = response {
+                    print("Your request returned an invalid response! Response: \(response)!")
+                } else {
+                    print("Your request returned an invalid response!")
+                }
+                return
+            }
+
+            /* GUARD: Was there any data returned? */
+            guard let data = data else {
+                print("No data was returned by the request!")
+                return
+            }
+
+            /* Parse the data! */
+            let parsedResult: AnyObject!
+            do {
+                parsedResult = try NSJSONSerialization.JSONObjectWithData(data, options: .AllowFragments)
+            } catch {
+                parsedResult = nil
+                print("Could not parse the data as JSON: '\(data)'")
+                return
+            }
+
+            /* GUARD: Did Flickr return an error (stat != ok)? */
+            guard let stat = parsedResult["stat"] as? String where stat == "ok" else {
+                print("Flickr API returned an error. See error code and message in \(parsedResult)")
+                return
+            }
+
+            /* GUARD: Is the "photos" key in our result? */
+            guard let photosDictionary = parsedResult["photos"] as? NSDictionary else {
+                print("Cannot find key 'photos' in \(parsedResult)")
+                return
+            }
+
+            /* GUARD: Is the "photo" key in photosDictionary? */
+            guard let photosArray = photosDictionary["photo"] as? [[String: AnyObject]] else {
+                print("Cannot find key 'photo' in \(photosDictionary)")
+                return
+            }
+
+            var imageURLs = [NSURL]()
+            for photoDictionary in photosArray {
+                if let imageUrlString = photoDictionary["url_m"] as? String {
+                    imageURLs.append(NSURL(string: imageUrlString)!)
+                }
+            }
+
+            if let completion = completion {
+                completion(imageURLs)
+            }
+        }
+        
+        task.resume()
+    }
+
     func createBoundingBoxString(latitude: Double, longitude: Double) -> String {
         /* Fix added to ensure box is bounded by minimum and maximums */
         let bottom_left_lon = max(longitude - BOUNDING_BOX_HALF_WIDTH, LON_MIN)
