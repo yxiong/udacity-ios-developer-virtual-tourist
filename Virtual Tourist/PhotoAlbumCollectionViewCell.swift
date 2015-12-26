3cfb9621f2f48bfba9c2262d6964c732a8db3fbb
Ying Xiong <yxiong@dropbox.com>
2015-12-26 10:18:02 -0800

    Setup photo album view

---
+++ Virtual Tourist/PhotoAlbumCollectionViewCell.swift
@@ -0,0 +1,13 @@
+//
+//  PhotoAlbumCollectionViewCell.swift
+//  Virtual Tourist
+//
+//  Created by Ying Xiong on 12/26/15.
+//  Copyright © 2015 Ying Xiong. All rights reserved.
+//
+
+import UIKit
+
+class PhotoAlbumCollectionViewCell: UICollectionViewCell {
+    @IBOutlet weak var imageView: UIImageView!
+}
--- Virtual Tourist/PhotoAlbumViewController.swift
+++ Virtual Tourist/PhotoAlbumViewController.swift
@@ -8,7 +8,36 @@
 
 import UIKit
 
-class PhotoAlbumViewController: UIViewController {
+class PhotoAlbumViewController: UIViewController, UICollectionViewDataSource {
+    @IBOutlet weak var collectionView: UICollectionView!
+    @IBOutlet weak var collectionViewFlowLayout: UICollectionViewFlowLayout!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        collectionView.dataSource = self
+
+        let space: CGFloat = 3.0
+        let dimension = (view.frame.size.width - (2 * space)) / 3.0
+        collectionViewFlowLayout.minimumInteritemSpacing = space
+        collectionViewFlowLayout.minimumLineSpacing = space
+        collectionViewFlowLayout.itemSize = CGSizeMake(dimension, dimension)
+    }
+
+    override func viewWillAppear(animated: Bool) {
+        super.viewWillAppear(animated)
+        collectionView.reloadData()
+    }
+
+    func collectionView(collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 15
+    }
+
+    func collectionView(collectionView: UICollectionView, cellForItemAtIndexPath indexPath: NSIndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCellWithReuseIdentifier("PhotoAlbumCollectionViewCell", forIndexPath: indexPath) as! PhotoAlbumCollectionViewCell
+        return cell
+    }
+
     @IBAction func backToMap(sender: AnyObject) {
         dismissViewControllerAnimated(true, completion: nil)
     }
